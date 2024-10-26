e3768bcab2ee8e4ba37df571ac98fc6ae1a33f3d
trant191 <tommytran1939@gmail.com>
2024-10-25 17:25:00 -0700

    add Generate SAS token, get sharable link
    1. adding Generate SAS token for 1 day
    2. remove asking file_path input
    3. return sharable link

--- clouduploader.sh
+++ clouduploader.sh
@@ -11,11 +11,12 @@ read -p "Enter your Azure Storage acount name: " AZURE_STORAGE_ACCOUNT
 read -sp "Enter you Azure Storage access key: " AZURE_STORAGE_KEY
 echo "..."
 read -p "Enter your container name: " my_container 
-read -p "Enter your file path: " file_path
+
 
 
 
 # Azure storage detail
+file_path="$1" #store file_path
 blob_name=$(basename "$file_path") # extract file name from path
 
 # Check if the required env var is set
@@ -24,9 +25,7 @@ if [ -z "$AZURE_STORAGE_ACCOUNT" ] || [ -z "$AZURE_STORAGE_KEY" ]; then
     exit 1
 fi
 
-
 # Upload file to azure Blob Storage
-
 echo "Uploading $file_path to Azure Blob Storage...."
 
 az storage blob upload \
@@ -36,9 +35,28 @@ az storage blob upload \
     --name "$blob_name" \
     --file "$file_path"
 
+# Check file upload status
 if [ $? -eq 0 ]; then
     echo "File uploaded successfully"
+    echo "Generating sharable link..."
+
+#set the expiration date for the SAS token
+    expiry=$(date -u -d "1 day" '+%Y-%m-%dT%H:%MZ')
+
+    sas_token=$(az storage blob generate-sas \
+        --account-name "$AZURE_STORAGE_ACCOUNT" \
+        --account-key "$AZURE_STORAGE_KEY" \
+        --container-name "$my_container" \
+        --name "$blob_name" \
+        --expiry "$expiry"\
+        --permission r\
+        --output tsv)
+
+    url="https://${AZURE_STORAGE_ACCOUNT}.blob.core.windows.net/${my_container}/$blob_name?$sas_token"
+    echo "Sharable URL Generated: $url"
 else
     echo "File upload error"
     exit 1
 fi
+
+
